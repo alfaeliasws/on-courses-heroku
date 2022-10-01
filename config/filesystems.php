2a25f44229a4f8327b586ec53697650acd8d1be3
alfaeliasws <alfaeliaswaristino@gmail.com>
2022-10-01 17:07:09 +0700

    file handling 2

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env('FILESYSTEM_DISK', 'links'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,6 +55,10 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+
+        'links' => [
+            base_path('public_html/storage') => storage_path('app/public'),
+        ]
 
     ],
 
